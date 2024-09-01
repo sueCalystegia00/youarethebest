79245dfb89b91d3f671e518e642043a4318a3d67
sueCalystegia00 <susan.basket0410@gmail.com>
2024-09-01 14:50:18 +0900

    add: contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	return <></>;
+	const [currentIndex, setCurrentIndex] = useState(-1);
+	const handleKeyPress = (event: { key: string }) => {
+		if (event.key === "1") {
+			setCurrentIndex(0);
+		} else if (event.key === "2") {
+			setCurrentIndex(1);
+		} else if (event.key === "3") {
+			setCurrentIndex(2);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("keydown", handleKeyPress);
+		return () => {
+			window.removeEventListener("keydown", handleKeyPress);
+		};
+	}, []);
+
+	return (
+		<div className='App'>
+			<div>
+				<img src='bar_master_mama_woman.png' alt='表示中の画像' />
+				{currentIndex === -1 ? (
+					<>
+						<p className='explain'>キーボードの1~3のどれかを押してください</p>
+					</>
+				) : (
+					<>
+						<p className='comment'>{commentsSet[currentIndex]}</p>
+					</>
+				)}
+			</div>
+		</div>
+	);
 }
+
+const commentsSet = [
+	"あんた 最高じゃないっ！",
+	"あんたって ホントすごいわね！",
+	"分かるわ 頑張ったのね！",
+];
 
 export default App;
